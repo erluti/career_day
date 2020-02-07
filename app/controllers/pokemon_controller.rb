1a4e8bde054bf1917c4284e479496dc545d21bcd
Philip Becker <philip.becker@oddball.io>
2020-02-07 00:23:09 -0500

    show names of counted pokemon

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,14 +1,14 @@
 class PokemonController < ApplicationController
   def index
-    @names = Pokemon.all.pluck(:name)
+    @names = Pokemon.pluck(:name)
   end
 
   def big_count
     # REVIEW make a "LetterCounter" class and show off tests?
     @letter = params[:letter]
     lower_letter = @letter.downcase
-    # REVIEW would like to have a "show them" button
-    @letter_count = Pokemon.all.pluck('name').sum {|name| name.count(lower_letter)}
+    @names = Pokemon.pluck(:name).select { |name| name.include?(lower_letter) }
+    @letter_count = @names.sum {|name| name.count(lower_letter)}
   end
 
   def by_word
