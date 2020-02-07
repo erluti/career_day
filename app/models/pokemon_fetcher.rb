87d9be6becf179de9d072ef1ffe77a365c07f6ad
Philip Becker <philip.becker@oddball.io>
2020-02-06 20:33:30 -0500

    fetch images

--- app/models/pokemon_fetcher.rb
+++ app/models/pokemon_fetcher.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'json'
 
+# This class is for consuming pokemon data from https://pokeapi.co/
 class PokemonFetcher
   def initialize
     @base_uri = 'https://pokeapi.co/api/v2/pokemon'
@@ -10,5 +11,24 @@ class PokemonFetcher
     res = Net::HTTP.get_response(URI("#{@base_uri}?limit=1000"))
     raise "Failed with #{res.class}!!" unless res.is_a?(Net::HTTPSuccess)
     JSON.parse(res.body)['results'].collect{ |element| element['name'] }
+  end
+
+  def image_path(name)
+    image_path = "/pokemon/#{name}.png"
+    explicit_path = Rails.root.to_s + '/app/assets/images' + image_path
+    unless File.exist?(explicit_path)
+      Rails.logger.info "Downloading image for '#{name}' and saving in '#{explicit_path}'."
+      # get image location
+      res = Net::HTTP.get_response(URI("#{@base_uri}/#{name}"))
+      raise "Failed to get #{name} with #{res.class}!!" unless res.is_a?(Net::HTTPSuccess)
+      image_url = JSON.parse(res.body)['sprites']['front_default']
+      # get image for display
+      res = Net::HTTP.get_response(URI(image_url))
+      raise "Failed to get #{name} with #{res.class}!!" unless res.is_a?(Net::HTTPSuccess)
+      open(explicit_path, "wb") do |file|
+        file.write(res.body)
+      end
+    end
+    image_path
   end
 end
