6ccf543dcd75a49f35d813adc83b7450ef053897
Philip Becker <philip.becker@oddball.io>
2020-02-06 19:47:00 -0500

    some styling updates and choosing any letter

--- app/controllers/simple_count_controller.rb
+++ app/controllers/simple_count_controller.rb
@@ -1,8 +1,14 @@
 class SimpleCountController < ApplicationController
   def letter_a
-    @reveal = params[:reveal]# == 'true'
+    @reveal = params[:reveal] == 'true'
   end
 
   def any_letter
+    @letter = params[:letter]
+    if @letter.present?
+      @letter_count = 'pikachu'.count(@letter.downcase)
+    else
+      @letter = '?'
+    end
   end
 end
